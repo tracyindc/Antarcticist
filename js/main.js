6ddc611751361155c751106e5648121e836ebe76
tracyindc <webteam@Shackleton2.local>
2014-06-12 00:28:43 -0400

    Adding js and form to OAE Page
    Added JS and FORM to OAE Page, tried to fix navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 });
+
+
+$(document).ready(function() {
+$("#Hillary").hide();
+$("#show_hide_Hillary").click(function() {
+$("#Hillary").toggle('slow');
+});
+});
+
+$(document).ready(function() {
+$("#Pinsetter").hide();
+$("#show_hide_Pinsetter").click(function() {
+$("#Hillary").toggle('slow');
+});
+});
+
+$(document).ready(function() {
+$("#Santa").hide();
+$("#show_hide_Santa").click(function() {
+$("#Santa").toggle('slow');
+});
+});
